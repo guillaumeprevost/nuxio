e46d90d9a295a5d6199b1784e556930d3af2dc6a
Guillaume Prevost <guillaume.prevost@live.com>
2023-02-17 19:22:42 +0100

    Bugfix : removed unused pics import

--- src/components/SecondaryFeatures.vue
+++ src/components/SecondaryFeatures.vue
@@ -58,9 +58,6 @@
 
 <script setup>
 import { TabGroup, TabList, Tab, TabPanels, TabPanel } from '@headlessui/vue'
-import screenshotContacts from '@/assets/images/screenshots/contacts.png'
-import screenshotInventory from '@/assets/images/screenshots/inventory.png'
-import screenshotProfitLoss from '@/assets/images/screenshots/profit-loss.png'
 
 const selectedIndex = ref(0);
 function changeTab(index) {
@@ -72,21 +69,21 @@ const features = [
     name: 'Reporting',
     summary: 'Stay on top of things with always up-to-date reporting features.',
     description: 'We talked about reporting in the section above but we needed three items here, so mentioning it one more time for posterity.',
-    image: screenshotProfitLoss,
+    image: '/screenshots/mockup-friendtheory.png',
     icon: resolveComponent('IconsReporting'),
   },
   {
     name: 'Inventory',
     summary: 'Never lose track of what’s in stock with accurate inventory tracking.',
     description: 'We don’t offer this as part of our software but that statement is inarguably true. Accurate inventory tracking would help you for sure.',
-    image: screenshotInventory,
+    image: '/screenshots/mockup-friendtheory.png',
     icon: resolveComponent('IconsInventory')
   },
   {
     name: 'Contacts',
     summary: 'Organize all of your contacts, service providers, and invoices in one place.',
     description: 'This also isn’t actually a feature, it’s just some friendly advice. We definitely recommend that you do this, you’ll feel really organized and professional.',
-    image: screenshotContacts,
+    image: '/screenshots/mockup-friendtheory.png',
     icon: resolveComponent('IconsContact')
   },
 ]
